d113f78d09e67ace55f683c0d8ee16076b52c472
Gabriel C. Netto <104046767+Gabrielcnetto@users.noreply.github.com>
2023-12-22 11:17:24 -0300

    e

--- lib/admScreens/components/fn/codeValidation.dart
+++ lib/admScreens/components/fn/codeValidation.dart
@@ -9,23 +9,13 @@ class CodeValidation extends StatefulWidget {
 }
 
 class _CodeValidationState extends State<CodeValidation> {
-  final numberClient = TextEditingController();
-  late String finalCode;
-  late int num1, num2, num3, num4, num5;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    numberClient.text = '12345'; // Exemplo de entrada
-    finalCode = numberClient.text;
-    num1 = int.parse(finalCode[0]);
-    num2 = int.parse(finalCode[1]);
-    num3 = int.parse(finalCode[2]);
-    num4 = int.parse(finalCode[3]);
-    num5 = int.parse(finalCode[4]);
-  }
+  final finalConfirmCode = TextEditingController();
+  final confirm1 = TextEditingController();
+  final confirm2 = TextEditingController();
+  final confirm3 = TextEditingController();
+  final confirm4 = TextEditingController();
+  final confirm5 = TextEditingController();
 
-  bool updateNumber = false;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -65,191 +55,141 @@ class _CodeValidationState extends State<CodeValidation> {
             SizedBox(
               height: 30,
             ),
-            InkWell(
-              onTap: () {},
-              child: Container(
-                alignment: Alignment.center,
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height / 4.5,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    width: 0.5,
-                    color: Colors.grey.shade900.withOpacity(0.5),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 4,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(
+                  width: 1,
+                  color: Colors.grey.shade900.withOpacity(0.3),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //Primeiro Número
+                  Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey.shade900.withOpacity(0.5),
+                      ),
+                    ),
+                    width: 60,
+                    height: 60,
+                    child: TextFormField(
+                      controller: confirm1,
+                      style: TextStyle(
+                        fontFamily: 'PoppinsTitle',
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                      ),
+                    ),
                   ),
-                  color: Colors.grey.shade100,
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.elliptical(15, 15),
-                          bottomRight: Radius.elliptical(15, 15),
-                          topLeft: Radius.elliptical(15, 15),
-                          topRight: Radius.elliptical(15, 15),
-                        ),
-                      ),
-                      elevation: 2,
-                      child: Container(
-                        padding: EdgeInsets.all(20),
-                        child: Text(
-                          '${num1}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'PoppinsTitle',
-                            fontSize: 16,
-                          ),
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.elliptical(15, 15),
-                            bottomRight: Radius.elliptical(15, 15),
-                            topLeft: Radius.elliptical(15, 15),
-                            topRight: Radius.elliptical(15, 15),
-                          ),
-                          border: Border.all(
-                            width: 0.5,
-                            color: Colors.grey.shade900.withOpacity(0.5),
-                          ),
-                        ),
+                  //Segundo Número
+                  Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey.shade900.withOpacity(0.5),
                       ),
                     ),
-                    Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.elliptical(15, 15),
-                          bottomRight: Radius.elliptical(15, 15),
-                          topLeft: Radius.elliptical(15, 15),
-                          topRight: Radius.elliptical(15, 15),
-                        ),
+                    width: 60,
+                    height: 60,
+                    child: TextFormField(
+                      controller: confirm2,
+                      style: TextStyle(
+                        fontFamily: 'PoppinsTitle',
+                        fontWeight: FontWeight.bold,
                       ),
-                      child: Container(
-                        padding: EdgeInsets.all(20),
-                        child: Text(
-                          '${num2}',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'PoppinsTitle',
-                          ),
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.elliptical(15, 15),
-                            bottomRight: Radius.elliptical(15, 15),
-                            topLeft: Radius.elliptical(15, 15),
-                            topRight: Radius.elliptical(15, 15),
-                          ),
-                          border: Border.all(
-                            width: 0.5,
-                            color: Colors.grey.shade900.withOpacity(0.5),
-                          ),
-                        ),
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
                       ),
                     ),
-                    Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.elliptical(15, 15),
-                          bottomRight: Radius.elliptical(15, 15),
-                          topLeft: Radius.elliptical(15, 15),
-                          topRight: Radius.elliptical(15, 15),
-                        ),
-                      ),
-                      child: Container(
-                        padding: EdgeInsets.all(20),
-                        child: Text(
-                          '${num3}',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'PoppinsTitle',
-                          ),
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.elliptical(15, 15),
-                            bottomRight: Radius.elliptical(15, 15),
-                            topLeft: Radius.elliptical(15, 15),
-                            topRight: Radius.elliptical(15, 15),
-                          ),
-                          border: Border.all(
-                            width: 0.5,
-                            color: Colors.grey.shade900.withOpacity(0.5),
-                          ),
-                        ),
+                  ),
+                  //terceiro número
+                  Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey.shade900.withOpacity(0.5),
                       ),
                     ),
-                    Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.elliptical(15, 15),
-                          bottomRight: Radius.elliptical(15, 15),
-                          topLeft: Radius.elliptical(15, 15),
-                          topRight: Radius.elliptical(15, 15),
-                        ),
+                    width: 60,
+                    height: 60,
+                    child: TextFormField(
+                      controller: confirm3,
+                      style: TextStyle(
+                        fontFamily: 'PoppinsTitle',
+                        fontWeight: FontWeight.bold,
                       ),
-                      child: Container(
-                        padding: EdgeInsets.all(20),
-                        child: Text(
-                          '${num4}',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'PoppinsTitle',
-                          ),
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.elliptical(15, 15),
-                            bottomRight: Radius.elliptical(15, 15),
-                            topLeft: Radius.elliptical(15, 15),
-                            topRight: Radius.elliptical(15, 15),
-                          ),
-                          border: Border.all(
-                            width: 0.5,
-                            color: Colors.grey.shade900.withOpacity(0.5),
-                          ),
-                        ),
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
                       ),
                     ),
-                    Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.elliptical(15, 15),
-                          bottomRight: Radius.elliptical(15, 15),
-                          topLeft: Radius.elliptical(15, 15),
-                          topRight: Radius.elliptical(15, 15),
-                        ),
-                      ),
-                      child: Container(
-                        padding: EdgeInsets.all(20),
-                        child: Text(
-                          '${num5}',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            fontFamily: 'PoppinsTitle',
-                          ),
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.elliptical(15, 15),
-                            bottomRight: Radius.elliptical(15, 15),
-                            topLeft: Radius.elliptical(15, 15),
-                            topRight: Radius.elliptical(15, 15),
-                          ),
-                          border: Border.all(
-                            width: 0.5,
-                            color: Colors.grey.shade900.withOpacity(0.5),
-                          ),
-                        ),
+                  ),
+                  //quarto número
+                  Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey.shade900.withOpacity(0.5),
                       ),
                     ),
-                  ],
-                ),
+                    width: 60,
+                    height: 60,
+                    child: TextFormField(
+                      controller: confirm4,
+                      style: TextStyle(
+                        fontFamily: 'PoppinsTitle',
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  //quinto número
+                  Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey.shade900.withOpacity(0.5),
+                      ),
+                    ),
+                    width: 60,
+                    height: 60,
+                    child: TextFormField(
+                      controller: confirm5,
+                      style: TextStyle(
+                        fontFamily: 'PoppinsTitle',
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
             SizedBox(
